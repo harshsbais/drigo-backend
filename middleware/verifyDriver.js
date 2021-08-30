87e689d2198d07e5f15331e0cea1f07f6ef0c54e
harshsbais <harshsbais5@gmail.com>
2021-08-30 20:47:31 +0530

    adds middleware for driver and user

---
+++ middleware/verifyDriver.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const Driver = require('../models/Driver');
+
+module.exports = async (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    res.status(403).send({
+      success: false,
+      message: 'Access Denied',
+    });
+  }
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const driver = Driver.findOne({ verified });
+    req.driver = driver;
+    next();
+  } catch (err) {
+    res.status(403).send({
+      success: false,
+      message: 'Access Denied',
+    });
+  }
+};
---
+++ middleware/verifyUser.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    res.status(403).send({
+      success: false,
+      message: 'Access Denied',
+    });
+  }
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const user = User.findOne({ verified });
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(403).send({
+      success: false,
+      message: 'Access Denied',
+    });
+  }
+};
