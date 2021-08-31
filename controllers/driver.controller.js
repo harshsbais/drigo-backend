4ce100fe9da8818ed2264ab72523c9a70b5c4765
harshsbais <harshsbais5@gmail.com>
2021-08-31 18:19:39 +0530

    refactors driver and bus routes into different controllers and routes

--- controllers/driver.controller.js
+++ controllers/driver.controller.js
@@ -1,18 +1,12 @@
 const Bus = require('../models/Bus');
 const Purchase = require('../models/Purchase');
+const Driver = require('../models/Driver');
 
-exports.addBus = async (req, res) => {
-  const {
-    source, timeTakenInMinutes, destination, price,
-  } = req.body;
+exports.getProfile = async (req, res) => {
   try {
-    const bus = new Bus({
-      source, timeTakenInMinutes, destination, price, driver: req.driver.id,
-    });
-    const savedBus = await bus.save();
     res.status(200).send({
       success: true,
-      savedBus,
+      driver: req.driver,
     });
   } catch (err) {
     res.status(500).send({
@@ -22,42 +16,12 @@ exports.addBus = async (req, res) => {
   }
 };
 
-exports.deleteBus = async (req, res) => {
+exports.updateProfile = async (req, res) => {
   try {
-    const bus = Bus.findOneAndDelete({ id: req.body.busID });
+    const savedDriver = await Driver.findOneAndUpdate({ id: req.body.id }, req.body);
     res.status(200).send({
       success: true,
-      message: 'Bus deleted successfully',
-    });
-  } catch (err) {
-    res.status(500).send({
-      success: false,
-      message: err.message,
-    });
-  }
-};
-
-exports.getBus = async (req, res) => {
-  try {
-    const buses = await Bus.findAll({ driver: req.body.driver.id });
-    res.status(200).send({
-      success: true,
-      buses,
-    });
-  } catch (err) {
-    res.status(500).send({
-      success: false,
-      message: err.message,
-    });
-  }
-};
-
-exports.editBus = async (req, res) => {
-  try {
-    const savedBus = await Bus.findOneAndUpdate({ id: req.body.id }, req.body);
-    res.status(200).send({
-      success: true,
-      savedBus,
+      savedDriver,
     });
   } catch (err) {
     res.status(500).send({
--- routes/driver.routes.js
+++ routes/driver.routes.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const verifyDriver = require('../middleware/verifyDriver');
 const {
-  getBus, getSales, editBus, deleteBus, addBus,
+  getSales, getProfile, updateProfile,
 } = require('../controllers/driver.controller');
 
-router.get('/bus', verifyDriver, getBus);
-router.post('/bus', verifyDriver, addBus);
-router.put('/bus', verifyDriver, editBus);
-router.delete('/bus', verifyDriver, deleteBus);
+router.get('/profile', verifyDriver, getProfile);
+router.put('/profile', updateProfile);
 router.get('/sales', verifyDriver, getSales);
 
 module.exports = router;
