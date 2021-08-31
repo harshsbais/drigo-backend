05799bd04355c750b0ebc513934fffc0ac5732b0
harshsbais <harshsbais5@gmail.com>
2021-08-31 16:59:03 +0530

    updates mongo query for edit and delete

--- routes/driver/deleteBus.js
+++ routes/driver/deleteBus.js
@@ -3,14 +3,7 @@ const Bus = require('../../models/Bus');
 
 router.delete('/', async (req, res) => {
   try {
-    const bus = Bus.find({ id: req.body.busID });
-    if (!bus) {
-      res.status(404).send({
-        success: false,
-        message: 'Bus not found',
-      });
-    }
-    const deletedBus = await bus.delete();
+    const bus = Bus.findOneAndDelete({ id: req.body.busID });
     res.status(204).send({
       success: true,
       message: 'Bus deleted successfully',
--- routes/driver/editBus.js
+++ routes/driver/editBus.js
@@ -3,9 +3,7 @@ const Bus = require('../../models/Bus');
 
 router.put('/', async (req, res) => {
   try {
-    let bus = await Bus.find({ id: req.body.id });
-    bus = { ...bus, ...req.body };
-    const savedBus = await bus.save();
+    const savedBus = await Bus.findOneAndUpdate({ id: req.body.id }, req.body);
     res.status(204).send({
       success: true,
       savedBus,
