9e1a15a802fa13ef2e7ffe83c050f2a7a1ab0ffc
harshsbais <harshsbais5@gmail.com>
2021-08-31 23:59:21 +0530

    seperates driver and user tests in diff files

--- tests/driver.spec.js
+++ tests/driver.spec.js
@@ -5,28 +5,6 @@ const testingData = require('./testingData');
 
 chai.should();
 chai.use(chaiHttp);
-describe('USER LOGIN', () => {
-  it('it should login with the provided credentials', (done) => {
-    chai
-      .request(server)
-      .post('/api/user/auth/login')
-      .set('content-type', 'application/json')
-      .set('accept', 'application/json')
-      .send(JSON.stringify(testingData.authDataUser))
-      .end((err, res) => {
-        if (err) {
-          console.log('got err login', err);
-          done();
-        }
-        console.log(res.body);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('success').eq(true);
-        res.body.should.have.property('token');
-        done();
-      });
-  });
-});
 
 describe('DRIVER LOGIN', () => {
   it('it should login with the provided credentials', (done) => {
---
+++ tests/user.spec.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../index');
+const testingData = require('./testingData');
+
+chai.should();
+chai.use(chaiHttp);
+describe('USER LOGIN', () => {
+  it('it should login with the provided credentials', (done) => {
+    chai
+      .request(server)
+      .post('/api/user/auth/login')
+      .set('content-type', 'application/json')
+      .set('accept', 'application/json')
+      .send(JSON.stringify(testingData.authDataUser))
+      .end((err, res) => {
+        if (err) {
+          console.log('got err login', err);
+          done();
+        }
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('success').eq(true);
+        res.body.should.have.property('token');
+        done();
+      });
+  });
+});
