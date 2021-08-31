05ee6164202471d656afbbe6179b002f16cfe772
harshsbais <harshsbais5@gmail.com>
2021-08-31 22:20:26 +0530

    adds mocha and chai testing for /login route

--- index.js
+++ index.js
@@ -24,3 +24,5 @@ app.use('/api/bus', busRoute);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+module.exports = app;
---
+++ tests/testingData.js
@@ -0,0 +1,8 @@
+exports.authDataUser = {
+  email: 'user-testing.@gmail.com',
+  password: '1234abcd',
+};
+exports.authDataDriver = {
+  email: 'driver-testing.@gmail.com',
+  password: '1234abcd',
+};
