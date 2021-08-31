3080611baca217302eba3e0d57d14f31b8e89cff
harshsbais <harshsbais5@gmail.com>
2021-08-31 23:59:44 +0530

    adds bus data for testing bus routes

--- tests/testingData.js
+++ tests/testingData.js
@@ -2,7 +2,20 @@ exports.authDataUser = {
   email: 'user-testing.@gmail.com',
   password: '1234abcd',
 };
+
 exports.authDataDriver = {
   email: 'driver-testing.@gmail.com',
   password: '1234abcd',
+};
+
+exports.addBusData = {
+  source: '612e6af3ddd6b7919d0149d5',
+  destination: '612e6b5e58d14516a0f94624',
+  price: 250,
+  timeTakenInMinutes: 450,
+};
+
+exports.editBusData = {
+  price: 450,
+  timeTakenInMinutes: 350,
 };
