e8f2aa038bd3c6f14ff7e6c5003e22f232f16fc5
harshsbais <harshsbais5@gmail.com>
2021-09-01 23:07:58 +0530

    updates purchase model

--- models/Purchase.js
+++ models/Purchase.js
@@ -3,20 +3,19 @@ const mongoose = require('mongoose');
 const PurchaseSchema = new mongoose.Schema({
   customer: {
     type: mongoose.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   driver: {
     type: mongoose.Types.ObjectId,
+    ref: 'Driver',
     required: true,
   },
   bus: {
     type: mongoose.Types.ObjectId,
+    ref: 'Bus',
     required: true,
   },
-  date: {
-    type: Date,
-    required: true,
-  },
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Purchase', PurchaseSchema);
