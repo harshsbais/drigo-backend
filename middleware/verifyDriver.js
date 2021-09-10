371305703b55fb3a22e880a45959d4b99389b928
harshsbais <harshsbais5@gmail.com>
2021-09-10 23:40:25 +0530

    updates middleware for verify driver and user

--- middleware/verifyDriver.js
+++ middleware/verifyDriver.js
@@ -12,7 +12,7 @@ module.exports = async (req, res, next) => {
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     const driver = await Driver.findOne({ id: verified });
-    req.body.driver = driver;
+    req.driver = driver;
     next();
   } catch (err) {
     res.status(403).send({
